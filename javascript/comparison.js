c508b8341160d4ddaa5621a2593d954a3353c14f
yuvraj-thesailor <yuvraj1904kansara@gmail.com>
2026-04-29 07:53:56 +0000

    comparison

---
+++ javascript/comparison.js
@@ -0,0 +1,22 @@
+//>,<,<=,>=,!=,==
+//different types of data types may show unpredictable results
+
+// console.log("2">1)
+// console.log("02"<3)
+// console.log("2a"==3)
+// console.log("2a"==null)
+
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)
+
+// console.log(undefined>=0)
+// console.log(undefined==0)//works differently than comparison operators
+// console.log(undefined>0)
+// console.log(undefined<0)
+
+// "===" strict checks, checks data types too
+// console.log("2"===2)
+// console.log("2"==2)
+
+//avoid this type of comparisons
