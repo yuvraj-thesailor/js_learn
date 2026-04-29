41fbb90dbdda3f903bb2f4e2d17cc3ff0fdf1f45
yuvraj-thesailor <yuvraj1904kansara@gmail.com>
2026-04-29 06:31:44 +0000

    operations

---
+++ javascript/operation.js
@@ -0,0 +1,22 @@
+//concatenation
+let str1 = "hi "
+let str2 = "mohan"
+// console.log(str1+str2)
+
+// console.log("1" + 2)
+// console.log(1+"2" +"1")
+// console.log("1"+1 +1)
+// console.log(1+1+"2")
+
+// console.log("2"*4*2)
+// console.log("2"+4*2)
+// console.log("2"+4+2*2)
+//need to go through conversion documentation
+
+//increment and assignment
+// let sum1=sum2=sum3=4
+// sum2= -4
+// console.table([sum1,sum2,sum3])
+// sum1++
+// +sum2
+// console.table([sum1,sum2,sum3])
